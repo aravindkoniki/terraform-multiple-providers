df69ed83083786b84f02f0350d6296f0dbd94991
Aravind Koniki <aravindkoniki@AK-PT-MacBook-Pro.local>
2025-04-04 13:08:53 +0400

    using profiles

--- providers.tf
+++ providers.tf
@@ -1,23 +1,18 @@
 provider "aws" {
   region  = "eu-west-1"
   alias   = "MY_DEV_ENVIRONMENT"
-  profile = var.my_dev_github_role != null ? null : "MY_DEV_ENVIRONMENT"
-  dynamic "assume_role" {
-    for_each = var.my_dev_github_role != null ? [var.my_dev_github_role] : []
-    content {
-      role_arn    = var.my_dev_github_role
-    }
-  }
+  profile = "MY_DEV_ENVIRONMENT"
 }
 
 provider "aws" {
   region  = "eu-west-1"
   alias   = "MY_DEVOPS"
-  profile = var.my_devops_github_role != null ? null : "MY_DEVOPS"
-  dynamic "assume_role" {
-    for_each = var.my_devops_github_role != null ? [var.my_devops_github_role] : []
-    content {
-      role_arn    = var.my_devops_github_role
-    }
-  }
+  profile = "MY_DEVOPS"
+  # profile = var.my_devops_github_role != null ? null : "MY_DEVOPS"
+  # dynamic "assume_role" {
+  #   for_each = var.my_devops_github_role != null ? [var.my_devops_github_role] : []
+  #   content {
+  #     role_arn    = var.my_devops_github_role
+  #   }
+  # }
 }
